73f84824e1d357333254b77f5c6acd5f9e573c2e
Carlos Segura <lithos.contact@gmail.com>
2023-02-09 09:02:17 +0100

    fix loader styles

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -4,7 +4,7 @@ export const Loader = () => {
   const { t } = useTranslation();
   return (
     <>
-      <div className="h-screen w-screen fixed z-50 top-0 left-0 opacity-100 backdrop-brightness-75 backdrop-blur duration-500" />
+      <div className="h-screen w-screen fixed z-50 top-0 left-0 duration-500" />
       <div className="z-50 h-screen w-screen fixed flex justify-center items-center">
         <div className="flex flex-col">
           <div className="lds-grid">
